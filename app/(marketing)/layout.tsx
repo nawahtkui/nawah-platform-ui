10549a6df1c0de70c507e9c13475a39ab0d7e5f8
nawahtkui <alnwyra@gmail.com>
2026-01-09 22:43:08 +0300

    add marketing layout

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,17 @@
+
+import MarketingNavbar from "../components/MarketingNavbar";
+import Footer from "../components/Footer";
+
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <MarketingNavbar />
+      {children}
+      <Footer />
+    </>
+  );
+}
